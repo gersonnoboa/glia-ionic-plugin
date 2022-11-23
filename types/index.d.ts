c67cfdd2d560ccf36566676d9bd089cf9c156a84
Gerson Noboa <gerson.noboa@glia.com>
2022-11-23 11:54:32 +0200

    Added type file

---
+++ types/index.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for glia-ionic-plugin
+// Project: https://github.com/gersonnoboa/glia-ionic-plugin
+// Definitions by: Microsoft Open Technologies Inc <http://msopentech.com>
+//                 Tim Brust <https://github.com/timbru31>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+interface GliaWidgets { }
+
+declare var GliaWidgets: GliaWidgets;
